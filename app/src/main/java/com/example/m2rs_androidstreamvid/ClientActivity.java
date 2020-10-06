f103cca000136a4c1eecd759834d6a78d173c3d0
GuillaumeDCRX <g.ducroux@outlook.fr>
2020-10-06 14:42:01 +0200

    discovery

--- app/src/main/java/com/example/m2rs_androidstreamvid/ClientActivity.java
+++ app/src/main/java/com/example/m2rs_androidstreamvid/ClientActivity.java
@@ -2,7 +2,16 @@ package com.example.m2rs_androidstreamvid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
 
 public class ClientActivity extends AppCompatActivity {
 
@@ -10,5 +19,37 @@ public class ClientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        registerReceiver(mReceiver, filter);
+        adapter.startDiscovery();
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)){
+
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+
+            }
+            else if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = (BluetoothDevice) intent .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String exemple = "Device found : " + device.getName();
+                Toast.makeText(getApplicationContext(), exemple, Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
