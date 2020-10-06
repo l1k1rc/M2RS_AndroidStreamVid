aa44d4e10602ad4d2e69bf0af9ec61b09ceae734
l1k1rc <throwaraccoon@gmail.com>
2020-10-06 12:50:27 +0200

    Add activities

--- app/src/main/java/com/example/m2rs_androidstreamvid/MainActivity.java
+++ app/src/main/java/com/example/m2rs_androidstreamvid/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.m2rs_androidstreamvid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button clientButton = (Button) findViewById(R.id.btnClient);
+        final Button serverButton = (Button) findViewById(R.id.btnServer);
+        clientButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.i("TAG", "TETS");
+                startActivity(new Intent(MainActivity.this, ClientActivity.class));
+            }
+        });
+        serverButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.i("TAG", "TETS");
+                startActivity(new Intent(MainActivity.this, ClientActivity.class));
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/m2rs_androidstreamvid/ServerActivity.java
@@ -0,0 +1,14 @@
+package com.example.m2rs_androidstreamvid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server);
+    }
+}
