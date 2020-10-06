b95513940892c692df3e6d87c7574e3948a51ecc
l1k1rc <throwaraccoon@gmail.com>
2020-10-06 14:56:09 +0200

    Add check privilege

--- app/src/main/java/com/example/m2rs_androidstreamvid/ServerActivity.java
+++ app/src/main/java/com/example/m2rs_androidstreamvid/ServerActivity.java
@@ -1,8 +1,20 @@
 package com.example.m2rs_androidstreamvid;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class ServerActivity extends AppCompatActivity {
 
@@ -10,5 +22,32 @@ public class ServerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
+
+        runtimePermission();
     }
+    /**
+     * To ask the permission to the user to have an access in his external storage
+     **/
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void runtimePermission() {
+        Log.i(MainActivity.TAG, "MainActivity onBind");
+        Dexter.withActivity(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+            @Override// if the permission is accepted, we launch the displaying of the music
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                Log.i(MainActivity.TAG, "Permission granted");
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).check();
+    }
+
 }
