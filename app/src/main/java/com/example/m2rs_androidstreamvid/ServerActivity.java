f59f4929a8b6b2403cf0e257d9a8b716d767b4bf
l1k1rc <throwaraccoon@gmail.com>
2020-10-06 21:42:37 +0200

    Add autotextview

--- app/src/main/java/com/example/m2rs_androidstreamvid/ServerActivity.java
+++ app/src/main/java/com/example/m2rs_androidstreamvid/ServerActivity.java
@@ -2,15 +2,12 @@ package com.example.m2rs_androidstreamvid;
 
 import android.Manifest;
 import android.annotation.TargetApi;
-import android.app.DownloadManager;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
@@ -22,15 +19,6 @@ import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 
 @SuppressWarnings("ALL")
 public class ServerActivity extends AppCompatActivity {
@@ -45,16 +33,16 @@ public class ServerActivity extends AppCompatActivity {
 
         final String downloadUrl = "https://ia800201.us.archive.org/22/items/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
         final Button btnDonwload = (Button) findViewById(R.id.btnDownload);
-
+        final MultiAutoCompleteTextView autoTextView = (MultiAutoCompleteTextView) findViewById(R.id.textViewUrl);
         runtimePermission();
 
         btnDonwload.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
                 Toast.makeText(ServerActivity.this, "Start downloading...", Toast.LENGTH_SHORT).show();
-                new DownloadFileFromURL().execute(downloadUrl);
+                //new DownloadFileFromURL().execute(downloadUrl);
 
-                Toast.makeText(ServerActivity.this, "File successfully downloaded..", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ServerActivity.this, autoTextView.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
