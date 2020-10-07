7f274c30e922d0fa4f2dffeea2362e2961677e40
GuillaumeDCRX <g.ducroux@outlook.fr>
2020-10-07 23:13:42 +0200

    add video view

--- app/src/main/java/com/example/m2rs_androidstreamvid/ClientSocketActivity.java
+++ app/src/main/java/com/example/m2rs_androidstreamvid/ClientSocketActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import java.io.File;
 
@@ -25,11 +27,29 @@ public class ClientSocketActivity extends AppCompatActivity {
         mBluetoothConnection = new ClientSocketTask(this, mDeviceAddress);
         mBluetoothConnection.execute();
 
-        newIntent.setAction(Intent.ACTION_SEND);
+        /*newIntent.setAction(Intent.ACTION_SEND);
         newIntent.setType("/");
         newIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File("downloadedfile.mp4")));
         Log.e(MainActivity.TAG,"INTENT USE");
-        startActivity(newIntent);
+        startActivity(newIntent);*/
+
+        // Get a reference to the VideoView instance as follows, using the id we set in the XML layout.
+        VideoView vidView = (VideoView)findViewById(R.id.video);
+
+        // Add playback controls.
+        MediaController vidControl = new MediaController(this);
+        // Set it to use the VideoView instance as its anchor.
+        vidControl.setAnchorView(vidView);
+        // Set it as the media controller for the VideoView object.
+        vidView.setMediaController(vidControl);
+
+        // Prepare the URI for the endpoint.
+        String vidAddress = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri vidUri = Uri.parse(vidAddress);
+        // Parse the address string as a URI so that we can pass it to the VideoView object.
+        vidView.setVideoURI(vidUri);
+        // Start playback.
+        vidView.start();
 
     }
 
